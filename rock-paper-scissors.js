a8b0bb61b7950ba85ab8308113ca62f72896fc1e
hmad213 <142343785+hmad213@users.noreply.github.com>
2024-12-10 16:13:43 +0500

    Add ui for rock paper scissors

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -2,54 +2,51 @@ function getComputerChoice(){
     randNum = Math.floor(Math.random() * 3)
     switch(randNum){
         case 0:
-            return "rock";
+            return "Rock";
             break;
         case 1:
-            return "paper";
+            return "Paper";
             break;
         case 2:
-            return "scissors";
-    }
-}
-
-function getHumanChoice(){
-    let userInput
-    userInput = prompt("Enter rock, paper, or scissors: ");
-    while(true){
-        userInput = userInput.toLowerCase()
-        if(userInput === "rock" || userInput === "paper" || userInput === "scissors"){
-            return userInput
-        }
-        userInput = prompt("Invalid input! Enter rock, paper, or scissors: ");
+            return "Scissors";
     }
 }
 
 function playGame(){
     function playRound(humanChoice, computerChoice){
         if(humanChoice === computerChoice){
-            alert("It's a draw. Both chose " + humanChoice);
+            div.textContent = `It's a draw. Both chose ${humanChoice}.User Score: ${humanScore}, Computer Score: ${computerScore}`;
         }
-        else if(humanChoice === "rock" && computerChoice === "paper" || humanChoice === "paper" && computerChoice === "scissors" || humanChoice === "scissors" && computerChoice === "rock"){
-            alert(`You lose! ${computerChoice} beats ${humanChoice}`);
+        else if(humanChoice === "Rock" && computerChoice === "Paper" || humanChoice === "Paper" && computerChoice === "Scissors" || humanChoice === "Scissors" && computerChoice === "Rock"){
             computerScore++;
+            div.textContent = `You lose! ${computerChoice} beats ${humanChoice}. User Score: ${humanScore}, Computer Score: ${computerScore}`;
         }
         else{
-            alert(`You win! ${humanChoice} beats ${computerChoice}`);
             humanScore++;
+            div.textContent = `You win! ${humanChoice} beats ${computerChoice}. User Score: ${humanScore}, Computer Score: ${computerScore}`;
         }
     }
-
-    let humanScore = 0;
+    
+    let div = document.querySelector("div");
     let computerScore = 0;
-
-    for(let i = 0; i < 5; i++){
-        let humanChoice = getHumanChoice();
-        let computerChoice = getComputerChoice();
-
-        playRound(humanChoice, computerChoice);
-    }
-    alert(`Your score is ${humanScore}`);
-    alert(`The computer's score is ${computerScore}`);
+    let humanScore = 0;
+    let gameEnd = false;
+    let buttons = document.querySelectorAll("button");
+    buttons.forEach((button) => {
+        button.addEventListener("click", (e) => {
+            playRound(e.target.textContent, getComputerChoice());
+            if(computerScore >= 5 || humanScore >= 5){
+                gameEnd = true;
+                if(computerScore >= 5){
+                    div.textContent = "Computer wins";
+                }
+                else{
+                    div.textContent = "User wins"
+                }
+                return;
+            }
+        })
+    })
 }
 
 playGame();
